aed369dc627e4ec141f3b158f9cdafa6e98f025b
Luis Martinez <rust@Luiss-Mac-mini.local>
2023-03-29 19:15:48 -0600

    Implemented tag model. Added tags API. Updated recipe endpoint to use tags.

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,6 +8,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagsViewSet)
 
 # for reverse mapping
 app_name = 'recipe'
